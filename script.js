8540de26a8f0b8986ed7bd97646db11ecb9bfe09
emihopebu <emihopebu@gmail.com>
2021-11-03 15:23:53 -0500

    Adding files

---
+++ script.js
@@ -0,0 +1,7 @@
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+
+function setDate() {
+  const now = new Date();
+}
