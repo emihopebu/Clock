7eea03c4371cbc85e9c7239c6450a27e203f6701
emihopebu <emihopebu@gmail.com>
2021-11-03 15:34:23 -0500

    Adding files

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ const second = document.querySelector(".second");
 
 function setDate() {
   const now = new Date();
+
+  const getSecond = now.getSeconds();
+  const getMinute = now.getMinutes();
+  const getHour = now.getHours();
+
+  const secondDegree = (getSecond / 60) * 360;
+  const minuteDegree = (getMinute / 60) * 360;
+  const hourDegree = (getHour / 12) * 360;
+
+  second.style.transform = `rotate(${secondDegree}deg)`;
+  minute.style.transform = `rotate(${minuteDegree}deg)`;
+  hour.style.transform = `rotate(${hourDegree}deg)`;
 }
+
+setInterval(setDate, 1000);
